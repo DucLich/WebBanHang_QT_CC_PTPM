17a66478086bad90546cc0ce34e1ec9ec3ab2b5d
VietUTT <xellcon0s@gmail.com>
2025-03-26 08:16:12 +0700

    Create list.php

---
+++ admin/thongke/list.php
@@ -0,0 +1,21 @@
+<div class="row">
+    <div class="row frmtitle">
+        <h1>THỐNG KÊ SẢN PHẨM THEO LOẠI</h1>
+    </div>
+    <div class="row frmcontent">
+        <div class="row mb frmdsloai">
+            <table>
+                <tr>
+                    <th>MÃ LOẠI</th>
+                    <th>LOẠI SẢN PHẨM</th>
+                    <th>SỐ LƯỢNG</th>
+                    <th>GIÁ CAO NHẤT</th>
+                    <th>GIÁ THẤP NHẤT</th>
+                    <th>GIÁ TRUNG BÌNH</th>
+                </tr>
+            </table>
+        </div>
+        <div class="row mb"><a href=""></a>
+        </div>
+    </div>
+</div>
